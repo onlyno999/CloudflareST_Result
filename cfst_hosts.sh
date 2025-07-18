035a2ba720b857bde0177a212466866889acfc12
onlyno999 <105115958+onlyno999@users.noreply.github.com>
2025-07-18 12:21:59 +0800

    Update cfst_hosts.sh

--- cfst_hosts.sh
+++ cfst_hosts.sh
@@ -2,64 +2,99 @@
 PATH=/bin:/sbin:/usr/bin:/usr/sbin:/usr/local/bin:/usr/local/sbin:~/bin
 export PATH
 # --------------------------------------------------------------
-#	项目: CloudflareSpeedTest 自动更新域名解析记录
-#	版本: 1.0.5
+#	[修改脚本信息]
+#	作者: Hervey
+#	修改说明: 支持同时修改 Hosts 中的 IPv4 及 IPv6 地址
+# --------------------------------------------------------------
+#	[原始脚本信息]
+#	项目: CloudflareSpeedTest 自动更新 Hosts
+#	版本: 1.0.4
 #	作者: XIU2
 #	项目: https://github.com/XIU2/CloudflareSpeedTest
 # --------------------------------------------------------------
 
-_READ() {
-	[[ ! -e "cfst_ddns.conf" ]] && echo -e "[错误] 配置文件不存在 [cfst_ddns.conf] !" && exit 1
-	CONFIG=$(cat "cfst_ddns.conf")
-	FOLDER=$(echo "${CONFIG}"|grep 'FOLDER='|awk -F '=' '{print $NF}')
-	[[ -z "${FOLDER}" ]] && echo -e "[错误] 缺少配置项 [FOLDER] !" && exit 1
-	ZONE_ID=$(echo "${CONFIG}"|grep 'ZONE_ID='|awk -F '=' '{print $NF}')
-	[[ -z "${ZONE_ID}" ]] && echo -e "[错误] 缺少配置项 [ZONE_ID] !" && exit 1
-	DNS_RECORDS_ID=$(echo "${CONFIG}"|grep 'DNS_RECORDS_ID='|awk -F '=' '{print $NF}')
-	[[ -z "${DNS_RECORDS_ID}" ]] && echo -e "[错误] 缺少配置项 [DNS_RECORDS_ID] !" && exit 1
-	KEY=$(echo "${CONFIG}"|grep 'KEY='|awk -F '=' '{print $NF}')
-	[[ -z "${KEY}" ]] && echo -e "[错误] 缺少配置项 [KEY] !" && exit 1
-	EMAIL=$(echo "${CONFIG}"|grep 'EMAIL='|awk -F '=' '{print $NF}')
-	[[ -z "${EMAIL}" ]] && echo -e "[信息] 缺少配置项 [EMAIL]，由 [API 密钥] 方式转为 [API 令牌] 方式!"
-	TYPE=$(echo "${CONFIG}"|grep 'TYPE='|awk -F '=' '{print $NF}')
-	[[ -z "${TYPE}" ]] && echo -e "[错误] 缺少配置项 [TYPE] !" && exit 1
-	NAME=$(echo "${CONFIG}"|grep 'NAME='|awk -F '=' '{print $NF}')
-	[[ -z "${NAME}" ]] && echo -e "[错误] 缺少配置项 [NAME] !" && exit 1
-	TTL=$(echo "${CONFIG}"|grep 'TTL='|awk -F '=' '{print $NF}')
-	[[ -z "${TTL}" ]] && echo -e "[错误] 缺少配置项 [TTL] !" && exit 1
-	PROXIED=$(echo "${CONFIG}"|grep 'PROXIED='|awk -F '=' '{print $NF}')
-	[[ -z "${PROXIED}" ]] && echo -e "[错误] 缺少配置项 [PROXIED] !" && exit 1
+_CHECK() {
+	while true
+		do
+		if [[ ! -e "nowip_hosts.txt" ]]; then
+			echo -e "该脚本的作用为 CloudflareST 测速后获取最快 IP 并替换 Hosts 中的 Cloudflare CDN IP。"
+			echo -e "使用前请先阅读相关使用说明："
+			echo -e "- https://github.com/zjjxwhh/OpenWrt-CloudflareSTHosts/blob/main/README.md"
+			echo -e "- https://github.com/XIU2/CloudflareSpeedTest/issues/42#issuecomment-768273848"
+			echo -e "第一次使用，请先将 Hosts 中所有 Cloudflare CDN IP 统一改为一个 IP。"
+			read -e -p "输入该 Cloudflare CDN IPv4 并回车（后续不再需要该步骤）：" NOWIPV4
+			if [[ ! -z "${NOWIPV4}" ]]; then
+				echo ${NOWIPV4} > nowip_hosts.txt
+			else
+				echo "该 IPv4 不能是空！"
+				break
+			fi
+
+			read -e -p "输入该 Cloudflare CDN IPv6 并回车（后续不再需要该步骤）：" NOWIPV6
+			if [[ ! -z "${NOWIPV6}" ]]; then
+				echo ${NOWIPV6} >> nowip_hosts.txt
+			else
+				echo "该 IPv6 不能是空！"
+				rm -f "nowip_hosts.txt"
+				break
+			fi
+		else
+			break
+		fi
+	done
 }
 
 _UPDATE() {
-	# 这里可以自己添加、修改 CFST 的运行参数
-	./cfst -o "result_ddns.txt"
+	echo -e "开始测速..."
+	NOWIPV4=$(head -1 nowip_hosts.txt)
+	NOWIPV6=$(tail -1 nowip_hosts.txt)
 
-	# 判断结果文件是否存在，如果不存在说明结果为 0
-	[[ ! -e "result_ddns.txt" ]] && echo "CFST 测速结果 IP 数量为 0，跳过下面步骤..." && exit 0
+	# 这里可以自己添加、修改 CloudflareST 的运行参数
+	cdnspeedtest -f "ip.txt" -o "result_hosts_ipv4.txt" -dd -t 1
 
-	CONTENT=$(sed -n "2,1p" result_ddns.txt | awk -F, '{print $1}')
-	if [[ -z "${CONTENT}" ]]; then
-		echo "CFST 测速结果 IP 数量为 0，跳过下面步骤..."
-		exit 0
-	fi
-	# 如果 EMAIL 变量是空的，那么就代表要使用 API 令牌方式
-	if [[ -n "${EMAIL}" ]]; then
-		# API 密钥方式（全局权限）
-		curl -X PUT "https://api.cloudflare.com/client/v4/zones/${ZONE_ID}/dns_records/${DNS_RECORDS_ID}" \
-			-H "X-Auth-Email: ${EMAIL}" \
-			-H "X-Auth-Key: ${KEY}" \
-			-H "Content-Type: application/json" \
-			--data "{\"type\":\"${TYPE}\",\"name\":\"${NAME}\",\"content\":\"${CONTENT}\",\"ttl\":${TTL},\"proxied\":${PROXIED}}"
+	# 如果需要 "找不到满足条件的 IP 就一直循环测速下去"，那么可以将下面的两个 exit 0 改为 _UPDATE 即可
+	[[ ! -e "result_hosts_ipv4.txt" ]] && echo "CloudflareST 测速结果 IPv4 数量为 0，跳过下面步骤..." && exit 0
+
+	cdnspeedtest -f "ipv6.txt" -o "result_hosts_ipv6.txt" -dd -t 1
+	[[ ! -e "result_hosts_ipv6.txt" ]] && echo "CloudflareST 测速结果 IPv6 数量为 0，跳过下面步骤..." && exit 0
+
+	# 下面这行代码是 "找不到满足条件的 IP 就一直循环测速下去" 才需要的代码
+	# 考虑到当指定了下载速度下限，但一个满足全部条件的 IP 都没找到时，CloudflareST 就会输出所有 IP 结果
+	# 因此当你指定 -sl 参数时，需要移除下面这段代码开头的 # 井号注释符，来做文件行数判断（比如下载测速数量：10 个，那么下面的值就设在为 11）
+	#[[ $(cat result_hosts.txt|wc -l) > 11 ]] && echo "CloudflareST 测速结果没有找到一个完全满足条件的 IP，重新测速..." && _UPDATE
+
+	BESTIPV4=$(sed -n "2,1p" result_hosts_ipv4.txt | awk -F, '{print $1}')
+	BESTIPV6=$(sed -n "2,1p" result_hosts_ipv6.txt | awk -F, '{print $1}')
+
+	echo "开始备份 Hosts 文件（myhosts_backup）..."
+	\cp -f /etc/myhosts /etc/CloudflareST/myhosts_backup
+
+	if [[ -z "${BESTIPV4}" ]]; then
+		echo "CloudflareST 测速结果 IPv4 数量为 0，跳过下面步骤..."
 	else
-		# API 令牌方式（自定义权限）
-		curl -X PUT "https://api.cloudflare.com/client/v4/zones/${ZONE_ID}/dns_records/${DNS_RECORDS_ID}" \
-			-H "Authorization: Bearer ${KEY}" \
-			-H "Content-Type: application/json" \
-			--data "{\"type\":\"${TYPE}\",\"name\":\"${NAME}\",\"content\":\"${CONTENT}\",\"ttl\":${TTL},\"proxied\":${PROXIED}}"
+		sed -i 's/'${NOWIPV4}'/'${BESTIPV4}'/g' nowip_hosts.txt
+		echo -e "\n旧 IPv4 为 ${NOWIPV4}\n新 IPv4 为 ${BESTIPV4}\n"
+
+		echo -e "开始替换 IPv4..."
+		sed -i 's/'${NOWIPV4}'/'${BESTIPV4}'/g' /etc/myhosts
+		echo -e "完成替换 IPv4..."
 	fi
+
+	if [[ -z "${BESTIPV6}" ]]; then
+		echo "CloudflareST 测速结果 IPv6 数量为 0，跳过下面步骤..."
+	else
+		sed -i 's/'${NOWIPV6}'/'${BESTIPV6}'/g' nowip_hosts.txt
+		echo -e "\n旧 IPv6 为 ${NOWIPV6}\n新 IPv6 为 ${BESTIPV6}\n"
+
+		echo -e "开始替换 IPv6..."
+		sed -i 's/'${NOWIPV6}'/'${BESTIPV6}'/g' /etc/myhosts
+		echo -e "完成替换 IPv6..."
+	fi
+
+	echo -e "开始重启 dnsmasq 服务..."
+	/etc/init.d/dnsmasq restart
+	echo -e "完成重启 dnsmasq 服务..."
 }
 
-_READ
-cd "${FOLDER}"
+_CHECK
 _UPDATE
